a385d43db5183be61d316b4182034aec59bbd1fd
Javier Revelo <shavi.sprint@gmail.com>
2023-08-15 00:21:29 -0500

    footer

--- proyecto-usabilidad/src/components/Footer.tsx
+++ proyecto-usabilidad/src/components/Footer.tsx
@@ -2,19 +2,21 @@ import React from 'react'
 
 const Footer = () => {
     return (
-        <div className="flex">
-            <div className="w-1/3 bg-gray-300 p-4">
-                Contenido de la primera columna
+        <footer className="flex w-full bg-slate-500 bottom-0">
+            <div className="w-1/3 p-4 text-left">
+                Construyendo un <br />servicio de justicia para <br /> la paz social
             </div>
 
-            <div className="w-1/3 bg-gray-400 p-4">
-                Contenido de la segunda columna
+            <div className="w-1/3 p-4 text-center">
+                CONTÁCTENOS <br /> 02 395 3600 / 023999800
+                EXT.: 25821
+                escuela.judicial@funcionjudicial.gob.ec
             </div>
 
-            <div className="w-1/3 bg-gray-500 p-4">
+            <div className="w-1/3 p-4 text-right">
                 Contenido de la tercera columna
             </div>
-        </div>
+        </footer>
     )
 }
 
