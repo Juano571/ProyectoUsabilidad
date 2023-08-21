dd2cdfe2b60a17083425b6b9fc0463bb4a05c7e1
Juan <juan.arellano@epn.edu.ec>
2023-08-20 23:25:21 -0500

    Footer json

--- proyecto-usabilidad/src/components/Footer.tsx
+++ proyecto-usabilidad/src/components/Footer.tsx
@@ -1,6 +1,23 @@
-import React from 'react'
+import { useEffect, useState } from 'react'
 
-const Footer = () => {
+const Footer: React.FC = () => {
+
+    // const [dataEs, setDataEs] = useState([])
+
+    // useEffect(() => {
+    //     fetch('proyecto-usabilidad\src\languages\es.json')
+    //         .then(response => response.json())
+    //         .then(data => {
+    //             setDataEs(data);
+    //         })
+    //         .catch(error => {
+    //             console.error("Error al leer el archivo JSON:", error);
+    //         });
+
+    // }, [])
+
+    // console.log(dataEs)
+
     return (
         <div className="flex">
             <div className="w-1/3 bg-gray-300 p-4">
