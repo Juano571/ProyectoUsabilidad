2d9c885166002f52afbe52c68201550b44b58426
Valery Vallejo <valery.antonella19@gmail.com>
2023-08-20 23:04:07 -0500

    Banner changes

--- proyecto-usabilidad/src/components/Banner.tsx
+++ proyecto-usabilidad/src/components/Banner.tsx
@@ -2,7 +2,16 @@ import React from 'react'
 
 const Banner = () => {
   return (
-    <div>Banner</div>
+    <div className='bg-slate-900'>
+      <div className='flex w-80%'>
+        <div>
+          <img src="/images/cdjlogo.svg" alt="Logo del Consejo de la Judicatura" />
+        </div>
+        <div className='flex text-white '>
+          <h1 className='text-4xl'>SISTEMA ÚNICO DE PENSIONES ALIMENTICIAS</h1>
+        </div>
+      </div>
+    </div>
   )
 }
 
