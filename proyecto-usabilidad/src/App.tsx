94e1e23289177a715fe24e2916612b954841c8e2
juanse <juan.posso@epn.edu.ec>
2023-08-21 01:20:55 -0500

    feat: Adding HelpGlossary

--- proyecto-usabilidad/src/App.tsx
+++ proyecto-usabilidad/src/App.tsx
@@ -4,6 +4,7 @@ import Banner from './components/Banner'
 import BodyLandingPage from './components/BodyLandingPage'
 import Help from './components/Help'
 import DetallesPensionAlimenticia from './components/DetallesPensionAlimenticia'
+import HelpGlossary from './components/HelpGlossary'
 
 function App() {
 
@@ -15,6 +16,7 @@ function App() {
       <BodyLandingPage/>
       <Footer />
       <Help />
+      <HelpGlossary />
     </>
   )
 }
---
+++ proyecto-usabilidad/src/components/HelpGlossary.tsx
@@ -0,0 +1,45 @@
+import React from 'react'
+
+const HelpGlossary = () => {
+    return (
+        <div>
+            <div> Ayuda </div>
+            <div>
+                <div>
+                    <h3> Glosario </h3>
+                </div>
+                <div>
+                    <img src="" alt="Imagen de un diccionario" />
+                </div>
+                <div>
+                    <details>
+                        <summary>Datos del representante legal</summary>
+                        <p>Definicion de datos de representante legal...............</p>
+                    </details>
+                    <details>
+                        <summary>Datos del deudor principal</summary>
+                        <p>Definicion de datos del deudor principal...............</p>
+                    </details>
+                    <details>
+                        <summary>Codigo de tarjeta </summary>
+                        <p>Definicion codigo de tarjeta...............</p>
+                    </details>
+                    <details>
+                        <summary>Codigo Imbabura</summary>
+                        <p>Definicion de codigo imbabura...............</p>
+                    </details>
+                    <details>
+                        <summary>No. proceso judicial</summary>
+                        <p>Definicion del numero proceso judicial...............</p>
+                    </details>
+                    <details>
+                        <summary>Identificacion nut</summary>
+                        <p>Definicion de datos de identificacion Nut...............</p>
+                    </details>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default HelpGlossary
