892923acba1d221ba8e5190b55b2da46b66935f8
Valery Vallejo <valery.antonella19@gmail.com>
2023-08-22 22:37:56 -0500

    Body Landing Page Styles v.1

--- proyecto-usabilidad/src/components/BodyLandingPage.tsx
+++ proyecto-usabilidad/src/components/BodyLandingPage.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import TableNoResults from './TableNoResults'
 import TableSearchResults from './TableSearchResults'
 import TableMovimientosPendientes from './TableInformacionGeneral'
@@ -6,27 +5,30 @@ import TableInformacionGeneral from './TableMovimientosPendientes2'
 
 const BodyLandingPage = () => {
     return (
-        <body>
-            <h1>Consulta de Tarjetas de Pensión Alimentaria</h1>
-            <label>Criterio de Búsqueda:</label>
-            <form action="POST">
-                <select>
-                    <option value="cedula">Cédula</option>
-                    <option value="codigo_tarjeta">Código de Tarjeta</option>
-                    <option value="proceso_judicial">Número de Proceso Judicial</option>
-                    <option value="codigo_imbabura">Código Anterior Imbabura</option>
-                    <option value="representante_legal">Datos del Representante Legal</option>
-                    <option value="deudor_principal">Datos del Deudor Principal</option>
-                    <option value="identificador_aprobacion">Identificador de Aprobación</option>
-                </select><br />
-                <label>Número de Cédula:</label>
-                <input type="text"  required/>
-                <button id="buscar">Buscar</button>
-            </form>
+        <body className='p-8'>
+            <h1 className='text-2xl text-[#245383]'>Consulta de Tarjetas de Pensión Alimentaria</h1>
+            <div className='flex py-10'>
+                <form action="POST">
+                    <div>
+                        <label className='text-lg'>Criterio de Búsqueda:</label>
+                        <select className='mx-8 px-2'>
+                            <option value="cedula">Cédula</option>
+                            <option value="codigo_tarjeta">Código de Tarjeta</option>
+                            <option value="proceso_judicial">Número de Proceso Judicial</option>
+                            <option value="codigo_imbabura">Código Anterior Imbabura</option>
+                            <option value="representante_legal">Datos del Representante Legal</option>
+                            <option value="deudor_principal">Datos del Deudor Principal</option>
+                            <option value="identificador_aprobacion">Identificador de Aprobación</option>
+                        </select>
+                    </div>
+                    <div className='mx-0'>
+                        <label className='text-lg'>Número de Cédula:</label>
+                        <input className='b' type="text"  required/>
+                        <button id="buscar">Buscar</button>
+                    </div>
+                </form>
+            </div>
             <TableNoResults/>
-            <TableSearchResults/>
-            <TableMovimientosPendientes/>
-            <TableInformacionGeneral/>
         </body>
     )
 }
