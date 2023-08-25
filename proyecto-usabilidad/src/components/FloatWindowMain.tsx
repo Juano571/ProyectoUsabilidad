78f6e3995de5aa4cb5c0f5eba0932a2719e0dda9
Javier Revelo <shavi.sprint@gmail.com>
2023-08-25 01:08:54 -0500

    Mejora funcion de combobox, ventanas flotantes y logra hacer que se cierra las ventanas

--- proyecto-usabilidad/src/components/FloatWindowMain.tsx
+++ proyecto-usabilidad/src/components/FloatWindowMain.tsx
@@ -1,12 +1,14 @@
-import React from 'react'
-import DetallesPensionAlimenticia from './DetallesPensionAlimenticia'
+import Indexaciones from './Indexaciones'
 
-const FloatWindow = () => {
+const FloatWindow = ({ closeModal }) => {
+  const stopPropagation = (event) => {
+    event.stopPropagation();
+  };
   return (
-    <div>
-        <div>
-            <DetallesPensionAlimenticia/>
-        </div>
+    <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50" onClick={closeModal}>
+      <div className='bg-white' onClick={stopPropagation}>
+        <Indexaciones closeModal={closeModal} />
+      </div>
     </div>
   )
 }
